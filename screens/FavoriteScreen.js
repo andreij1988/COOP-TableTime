419f3218395d0f32a68a4ab796f08a13b046d1c4
mabuhannood <122485641+mabuhannood@users.noreply.github.com>
2023-11-14 23:42:21 -0500

    updating the favorite screen

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,16 +1,39 @@
-// FavoriteScreen.js
-
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const FavoriteScreen = () => {
+  const favoriteData = [
+    {
+      id: "1",
+      restaurantName: "Favorite Restaurant 1",
+      guestName: "John",
+      guestCount: 2,
+      dineTime: "19:00",
+    },
+    {
+      id: "2",
+      restaurantName: "Favorite Restaurant 2",
+      guestName: "Alice",
+      guestCount: 4,
+      dineTime: "20:30",
+    },
+  ];
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Favorite Screen</Text>
-      {/* Display fixed data */}
-      <Text style={styles.favoriteItem}>Favorite Item 1</Text>
-      <Text style={styles.favoriteItem}>Favorite Item 2</Text>
-      {/* Add more items as needed */}
+      <FlatList
+        style={styles.list}
+        data={favoriteData}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.reservationItem}>
+            <View style={styles.reservationDetails}>
+              <Text style={styles.detailLabel}>Name:</Text>
+              <Text>{item.restaurantName}</Text>
+            </View>
+          </View>
+        )}
+      />
     </View>
   );
 };
@@ -18,17 +41,30 @@ const FavoriteScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#fff",
+    padding: 20,
   },
   title: {
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 20,
   },
-  favoriteItem: {
-    fontSize: 18,
-    marginBottom: 10,
+  list: {
+    flex: 1,
+  },
+  reservationItem: {
+    borderBottomWidth: 1,
+    borderBottomColor: "blue",
+    paddingBottom: 15,
+    marginBottom: 15,
+  },
+  reservationDetails: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 5,
+  },
+  detailLabel: {
+    fontWeight: "bold",
   },
 });
 
